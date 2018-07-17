85812ffb4e1490940e4e4782b70ae992f621e78e
leesangyoon <pinetree408@gmail.com>
2018-07-17 18:29:01 +0900

    Update Controller logic & Add View binding interface

--- app/src/main/java/com/pinetree408/study/tictactoe/controller/TicTacToeActivity.java
+++ app/src/main/java/com/pinetree408/study/tictactoe/controller/TicTacToeActivity.java
@@ -1,32 +1,48 @@
 package com.pinetree408.study.tictactoe.controller;
 
-import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import com.pinetree408.study.tictactoe.R;
+import com.pinetree408.study.tictactoe.databinding.TictactoeBinding;
 import com.pinetree408.study.tictactoe.model.Board;
+import com.pinetree408.study.tictactoe.model.Player;
 
 
 public class TicTacToeActivity extends AppCompatActivity {
 
-    private static String TAG = TicTacToeActivity.class.getName();
-
     private Board model;
+
+    private TictactoeBinding viewBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.tictactoe);
 
+        viewBinding = DataBindingUtil.setContentView(this, R.layout.tictactoe);
+
         model = new Board();
     }
 
-    public void onCellClicked(View button) {
+    public void onCellClicked(View v) {
+        Button button = (Button) v;
+
         String tag = button.getTag().toString();
         int row = Integer.valueOf(tag.substring(0,1));
         int col = Integer.valueOf(tag.substring(1,2));
-        Log.i(TAG, "Click Row: [" + row + "," + col + "]");
+
+        Player playerThatMoved = model.mark(row, col);
+
+        if(playerThatMoved != null) {
+            button.setText(playerThatMoved.toString());
+            if (model.getWinner() != null) {
+                viewBinding.winnerPlayerLabel.setText(playerThatMoved.toString());
+                viewBinding.winnerPlayerViewGroup.setVisibility(View.VISIBLE);
+            }
+        }
     }
 }
